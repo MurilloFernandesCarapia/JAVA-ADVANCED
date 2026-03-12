25a75a5648e165a7b6b3ab7745cc9ad59a982910
MurilloFernandesCarapia <murilincarapia@gmail.com>
2026-03-11 23:15:29 -0300

    Fazendo um construction injection no projeto ao inves do field

---
+++ api-rest-pw/src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
@@ -0,0 +1,65 @@
+package br.com.fiap.api_rest.controller;
+
+import br.com.fiap.api_rest.dto.ProdutoRequest;
+import br.com.fiap.api_rest.dto.ProdutoResponse;
+import br.com.fiap.api_rest.model.Produto;
+import br.com.fiap.api_rest.service.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> createProduto(@Valid @RequestBody ProdutoRequest produto) {
+        Produto produtoSalvo = produtoService.create(produto);
+        return new ResponseEntity<>(produtoSalvo, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoResponse> readProduto(@PathVariable UUID id) {
+        ProdutoResponse produto = produtoService.read(id);
+        if (produto == null) {
+            return new ResponseEntity<>(produto, HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(produto, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoResponse>> readProduto() {
+        List<ProdutoResponse> produtos = produtoService.read();
+        if (produtos.isEmpty()) {
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(produtoService.read(), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto) {
+        ProdutoResponse produtoExistente = produtoService.read(produto.getId());
+        if (produtoExistente == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Produto produtoAtualizado = produtoService.update(produto);
+        return new ResponseEntity<>(produtoAtualizado, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduto(@PathVariable UUID id) {
+        produtoService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ api-rest-pw/src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
@@ -0,0 +1,74 @@
+package br.com.fiap.api_rest.service;
+
+import br.com.fiap.api_rest.dto.ProdutoRequest;
+import br.com.fiap.api_rest.dto.ProdutoResponse;
+import br.com.fiap.api_rest.mapper.ProdutoMapper;
+import br.com.fiap.api_rest.model.Produto;
+import br.com.fiap.api_rest.repository.ProdutoRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+    private final ProdutoMapper produtoMapper;
+
+
+    @Autowired
+
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoMapper produtoMapper){
+        this.produtoRepository = produtoRepository;
+        this.produtoMapper = produtoMapper;
+    }
+
+    // CRUD
+    public Produto create(ProdutoRequest produtoRequest) {
+        Produto produto = new Produto();
+        BeanUtils.copyProperties(produtoRequest, produto);
+        return produtoRepository.save(produto);
+    }
+
+    public ProdutoResponse read(UUID id) {
+        Optional<Produto> produto =  produtoRepository.findById(id);
+        if(produto.isEmpty()){
+            return null;
+        }
+        return produtoMapper.produtoToResponse((produto.get()));
+
+    }
+
+
+
+    public List<ProdutoResponse> read() {
+        List<Produto> produtos = produtoRepository.findAll();
+        return produtos.stream().map(produtoMapper::produtoToResponse).collect(Collectors.toList());
+    }
+
+    /*
+    public List<ProdutoResponse> read2() {
+        List<Produto> produtos = produtoRepository.findAll();
+        List<ProdutoResponse> produtosResponse = new ArrayList<>();
+        for (Produto produto : produtos){
+            produtosResponse.add(produtoMapper.produtoToResponse(produto));
+        }
+        return produtosResponse;
+    }
+
+    */
+
+    public Produto update(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public void delete(UUID id) {
+        produtoRepository.deleteById(id);
+    }
+}
